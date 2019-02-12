5674768f023c8cf2ffbafb044744ec5a5993d523
Manoj K <47323583+manojmjofficial@users.noreply.github.com>
2019-02-12 20:25:37 +0530

    Create OddEven.py

---
+++ OddEven.py
@@ -0,0 +1,8 @@
+//This program checks whether the given number is odd or even.
+a=int(input("Enter a number:"))
+if(a==0):
+    print("Neither Even nor Odd")
+elif(a%2==0):
+    print(a," is Even")
+else:
+    print(a," is Odd")
