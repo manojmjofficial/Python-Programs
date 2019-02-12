47ae14a3e77a17d7e3205108aeb947daf9f3f73b
Manoj K <47323583+manojmjofficial@users.noreply.github.com>
2019-02-12 20:06:29 +0530

    Create CheckSign.py

---
+++ CheckSign.py
@@ -0,0 +1,7 @@
+a = float(input("Enter a number: "))
+if a > 0:
+   print(a," is a Positive Integer")
+elif a == 0:
+   print("The Given number is a Zero")
+else:
+   print(a," is a Negative Integer")
