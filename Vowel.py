789028a5babecfa2d92df30d9d90469b50f424a8
Manoj K <47323583+manojmjofficial@users.noreply.github.com>
2019-02-12 20:35:26 +0530

    Create Vowel.py

---
+++ Vowel.py
@@ -0,0 +1,7 @@
+#This Program checks whether the given character is a Vowel or Consonant.
+a=input("Enter a Letter:")
+a=a.lower()
+if(a=='a' or a=='e' or a=='i' or a=='o' or a=='u'):
+    print("It is a Vowel")
+else:
+    print("It is a Consonant")
